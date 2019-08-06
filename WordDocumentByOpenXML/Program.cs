0d26ae099372916113aca483606f68a83aab197c
sergeklokov <serge.klokov@gmail.com>
2019-08-06 08:05:48 -0700

    Add project files.

---
+++ WordDocumentByOpenXML/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+//using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace WordDocumentByOpenXML
+{
+    /// <summary>
+    /// TODO:
+    /// Export to the Word DOCX document
+    /// Based on tebmplate with tables
+    /// 
+    /// Serge Klokov 2019
+    /// </summary>
+    class Program
+    {
+        const string fileName = "test.docx";
+        static void Main(string[] args)
+        {
+            using (var wordDocument = WordprocessingDocument.Create(fileName, WordprocessingDocumentType.Document))
+            Console.WriteLine("");
+        }
+    }
+}
