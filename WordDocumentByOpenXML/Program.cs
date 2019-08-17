c64eccabcbf813a12d7bb97d8dcd73bed72015ef
sergeklokov <serge.klokov@gmail.com>
2019-08-17 16:27:34 -0700

    added conversion template to stream without saving to temp file

--- WordDocumentByOpenXML/Program.cs
+++ WordDocumentByOpenXML/Program.cs
@@ -26,17 +26,22 @@ namespace WordDocumentByOpenXML
     /// </summary>
     public class Program
     {
-        const string templateFileName = "Template.dotx";
+        const string templateFileName = "Letter.docx";
+        //const string templateFileName = "Template.dotx";
         const string docFileNameWTemplate = "ExportedBasedOnTemplate.docx";
         const string docFileName = "ExportedData.docx";
 
         // key values on the left if present in the Word Template, 
         // will be replaced by text from values
         static Dictionary<string, string> textData = new Dictionary<string, string>() {
-            ["TextDataName"] = "Sergiy Klokov",
-            ["TextDataAddress"] = "6200 E Germann Rd, Chandler, AZ",
-            ["TextDataToday"] = DateTime.Now.ToLongDateString(),
-            ["TextDataDOB"] = "10/03/1800",
+            ["DataNameFrom"] = "Sergiy Klokov",
+            ["DataToday"] = DateTime.Now.ToLongDateString(),
+            ["DataDOB"] = "10/03/1800",
+            ["DataAddressFrom"] = "200 S Cloud Rd",
+            ["DataCityZipFrom"] = "Chandler, AZ, 85249",
+            ["DataNameTo"] = "McKayla Klokov",
+            ["DataAddressTo"] = "6200 E Germann Rd",
+            ["DataCityZipTo"] = "Chandler, AZ, 85286",
             //[""] = "",
         };
 
@@ -89,5 +94,65 @@ namespace WordDocumentByOpenXML
 
             }
         }
+
+        public static MemoryStream GetWordDocumentFromTemplateWithTempFile()
+        {
+            string tempFileName = Path.GetTempFileName();
+            var templatePath = AppDomain.CurrentDomain.BaseDirectory + @"Controllers\" + templateFileName;
+
+            using (var document = WordprocessingDocument.CreateFromTemplate(templatePath))
+            {
+                var body = document.MainDocumentPart.Document.Body;
+
+                //add some text 
+                Paragraph paraHeader = body.AppendChild(new Paragraph());
+                Run run = paraHeader.AppendChild(new Run());
+                run.AppendChild(new Text("This is body text"));
+
+                OpenXmlPackage savedDoc = document.SaveAs(tempFileName); // Save result document, not modifying the template
+                savedDoc.Close();  // can't read if it's open
+                document.Close();
+            }
+
+            var memoryStream = new MemoryStream(File.ReadAllBytes(tempFileName)); // this works but I want to avoid saving and reading file
+
+            //memoryStream.Position = 0; // should I rewind it? 
+            return memoryStream;
+        }
+
+
+        /// <summary>
+        /// Trick is to open template for editing, 
+        /// then change type to the document
+        /// Open method have returned stream
+        /// </summary>
+        /// <returns></returns>
+        public static MemoryStream GetWordDocumentStreamFromTemplate()
+        {
+            var templatePath = AppDomain.CurrentDomain.BaseDirectory + @"Controllers\" + templateFileName;
+            var memoryStream = new MemoryStream();
+
+            using (var fileStream = new FileStream(templatePath, FileMode.Open, FileAccess.Read))
+                fileStream.CopyTo(memoryStream);
+
+            using (var document = WordprocessingDocument.Open(memoryStream, true))
+            {
+                document.ChangeDocumentType(WordprocessingDocumentType.Document); // change from template to document
+
+                var body = document.MainDocumentPart.Document.Body;
+
+                //add some text 
+                Paragraph paraHeader = body.AppendChild(new Paragraph());
+                Run run = paraHeader.AppendChild(new Run());
+                run.AppendChild(new Text("This is body text"));
+
+                document.Close();
+            }
+
+            memoryStream.Position = 0; //let's rewind it
+            return memoryStream;
+        }
     }
+
+
 }
